d86d3aa383bed0a9d9dca0cd0c1a1452ea7a5446
BrianWeiss1 <brianweissct@gmail.com>
2024-12-02 00:32:02 -0500

    Fixed code

--- December/2/Question1&2.py
+++ December/2/Question1&2.py
@@ -10,9 +10,6 @@ while data != "":
         lst1.append(listData)
     data = f.readline()
 
-print(lst1)
-# lst1.sort()
-# lst2.sort()
 
 
 # Problem 1
@@ -39,16 +36,41 @@ for j in range(len(lst1)):
             
     if valid == True:
         count+=1
+        
+print(count)
+count = 0
+        
+        
+#Problem 2
+decreasing = False
+increasing = False
+valid = True
+count = 0
+for j in range(len(lst1)):
+    valid = True
+    decreasing = False
+    increasing = False
+    continueBreak = False
+    for i in range(len(lst1[j])-1):
+        if decreasing and float(lst1[j][i]) < float(lst1[j][i+1]):
+            valid = False
+        elif increasing and float(lst1[j][i]) > float(lst1[j][i+1]):
+            valid = False
+        if float(lst1[j][i]) > float(lst1[j][i+1]):
+            decreasing = True
+        elif float(lst1[j][i]) < float(lst1[j][i+1]):
+            increasing = True
+        if not (abs(float(lst1[j][i])-float(lst1[j][i+1])) >= 1 and abs(float(lst1[j][i])-float(lst1[j][i+1])) <= 3):
+            valid = False
+            
+    if valid == True:
+        count+=1
     else:
-        # lstTest = []
         for skip in range(len(lst1[j])):
-            print(lst1[j])
             lstTest = []
-        # for skip in range(len([1, 3, 2, 4, 5])):
             valid = True
             decreasing = False
             increasing = False
-
             continueBreak = False
             lstTest = lst1[j][0:skip] + lst1[j][skip+1:len(lst1[j])]
             for i in range(len(lstTest)-1):
@@ -68,8 +90,5 @@ for j in range(len(lst1)):
                 count+=1
                 break 
             
-
-        
         
 print(count)
-# Problem 2
