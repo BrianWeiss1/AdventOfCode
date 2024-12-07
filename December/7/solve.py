96d7f66428d3538b576c56f0faabe57a057ec6d3
BrianWeiss1 <brianweissct@gmail.com>
2024-12-07 01:14:23 -0500

    added problem 1

--- December/7/solve.py
+++ December/7/solve.py
@@ -13,6 +13,54 @@ for set in normalList:
     test = set[1]
     test = test.split(" ")[1:]
     multiplicationTest = True
+    permutations = list(product(["multi", "add"], repeat=len(test)-1))
+    for perm in permutations:
+        currentConcat = []
+        fakeTest = test
+        total = int(fakeTest[0])
+        if int(set[0]) == 7290:
+            if perm == ('add', 'multi'):
+                pass
+
+        for i in range(len(test)-1):
+            if perm[i] == "multi": 
+                total*=int(fakeTest[i+1])
+            elif perm[i] == "add":
+                total+=int(fakeTest[i+1])
+            elif perm[i] == "concat":
+                totalStr = ""
+                totalStr = totalStr + str(total)
+                totalStr = totalStr + str(fakeTest[i+1])
+
+                total = int(totalStr)
+        if len(currentConcat) != 0:
+            currentConcat.append(total)
+            totalStr = ""
+            for concatStuff in currentConcat:
+                totalStr = totalStr + str(concatStuff) 
+            if int(totalStr) == int(answer):
+                sumLists+=int(answer)
+                break
+        else:
+            if total == int(answer):
+                sumLists+=total
+                break
+
+print(sumLists)
+                
+        
+
+
+
+
+#Problem 2
+sumLists = 0
+for set in normalList:
+    set = set.split(':')
+    answer = set[0]
+    test = set[1]
+    test = test.split(" ")[1:]
+    multiplicationTest = True
     permutations = list(product(["multi", "add", 'concat'], repeat=len(test)-1))
     for perm in permutations:
         currentConcat = []
